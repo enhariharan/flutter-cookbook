7823fe6e3c27631579f3546fc1cb7666f92e2c6e
Hariharan Narayanan <enhariharan@gmail.com>
2025-12-27 08:10:00 +0530

    chapter 5 - initialized Flutter project _03_custom_paint featuring CustomPaint star rating and profile screen layout

---
+++ ch_05/_03_custom_paint/android/app/src/main/kotlin/com/example/u_03_custom_paint/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.u_03_custom_paint
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ ch_05/_03_custom_paint/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'profile_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Placing widgets Demo',
+      theme: ThemeData(
+        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const ProfileScreen(),
+    );
+  }
+}
