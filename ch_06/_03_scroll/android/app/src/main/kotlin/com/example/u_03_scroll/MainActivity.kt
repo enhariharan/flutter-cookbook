eeea31460e42e8fde946a41e0bc70a85c77f6ce7
Hariharan Narayanan <enhariharan@gmail.com>
2025-12-29 22:10:33 +0530

    chapter 6 - initialized Flutter project _03_scroll and implemented a StopWatch screen with lap recording functionality and a scrollable ListView display

---
+++ ch_06/_03_scroll/android/app/src/main/kotlin/com/example/u_03_scroll/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.u_03_scroll
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ ch_06/_03_scroll/lib/stop_watch.dart
@@ -0,0 +1,149 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+class MyStopWatch extends StatefulWidget {
+  const MyStopWatch({super.key});
+
+  @override
+  State<MyStopWatch> createState() => _MyStopWatchState();
+}
+
+class _MyStopWatchState extends State<MyStopWatch> {
+  int milliseconds = 0;
+  late Timer timer;
+  bool isTicking = false;
+  final laps = <int>[];
+
+  void _startTimer() {
+    setState( () {
+      timer = Timer.periodic(const Duration(milliseconds: 100), _onTick);
+      milliseconds = 0;
+      isTicking = true;
+      laps.clear();
+    });
+  }
+
+  void _stopTimer() {
+    timer.cancel();
+    setState(() => isTicking = false);
+  }
+
+  void _onTick(Timer time) {
+    if (mounted) {
+      setState(() => milliseconds += 100);
+    }
+  }
+
+  void _lap() {
+    setState(() {
+      laps.add(milliseconds);
+      milliseconds = 0;
+    });
+  }
+
+  @override
+  void dispose() {
+    timer.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('StopWatch'),
+      ),
+      body: Column(
+        children: [
+          Expanded(child: _buildCounter(context)),
+          Expanded(child: _buildLapDisplay(context)),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildLapDisplay(BuildContext context) {
+    return ListView(
+      children: [
+        for (int milliseconds in laps)
+          ListTile(
+            title: Text(_secondsText(milliseconds)),
+          )
+      ],
+    );
+  }
+
+  Widget _buildCounter(BuildContext context) {
+    return Container(
+      color: Theme.of(context).primaryColor,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            'Lap ${laps.length + 1}',
+            style: Theme.of(context).textTheme.headlineSmall?.copyWith(color: Colors.white),
+          ),
+          Text(
+            _secondsText(milliseconds),
+            style: Theme.of(context).textTheme.headlineSmall?.copyWith(color: Colors.white),
+          ),
+          const SizedBox(height: 20,),
+          _buildControls(),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildControls() {
+    return Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            _buildStartButton(),
+            const SizedBox(width: 20,),
+            _buildStopButton(),
+            const SizedBox(width: 20,),
+            _buildLapButton(),
+            const SizedBox(width: 20,),
+          ],
+        );
+  }
+
+  Widget _buildLapButton() {
+    return ElevatedButton(
+      style: ButtonStyle(
+        backgroundColor: MaterialStateProperty.all<Color>(Colors.yellow),
+        foregroundColor: MaterialStateProperty.all<Color>(Colors.black),
+      ),
+      onPressed: isTicking ? _lap : null,
+      child: const Text('Lap'),
+    );
+  }
+
+  Widget _buildStopButton() {
+    return TextButton(
+            style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all<Color>(Colors.red),
+              foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+            ),
+            onPressed: isTicking ? _stopTimer : null,
+            child: const Text('Stop'),
+          );
+  }
+
+  Widget _buildStartButton() {
+    return ElevatedButton(
+            style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all<Color>(Colors.green),
+              foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+            ),
+            onPressed: isTicking ? null : _startTimer,
+            child: const Text('Start'),
+          );
+  }
+
+  String _secondsText(int milliseconds) {
+    final seconds = milliseconds / 1000;
+    return '$seconds seconds';
+  }
+}
