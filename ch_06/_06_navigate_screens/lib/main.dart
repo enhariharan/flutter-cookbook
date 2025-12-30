0e0641db734f53fe3700b58b5028a3f2556e0f16
Hariharan Narayanan <enhariharan@gmail.com>
2025-12-30 22:10:07 +0530

    chapter 6 - initialized Flutter project _06_navigate_screens and implemented multi-screen navigation between Login and StopWatch screens

---
+++ ch_06/_06_navigate_screens/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'stop_watch.dart';
+import 'login_screen.dart';
+
+void main() => runApp(const StopWatchApp());
+
+class StopWatchApp extends StatelessWidget {
+  const StopWatchApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: LoginScreen(),
+    );
+  }
+}
