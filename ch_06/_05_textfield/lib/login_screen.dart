4e18ddd5ecd3433887345b759e015ad92af5a548
Hariharan Narayanan <enhariharan@gmail.com>
2025-12-30 21:07:58 +0530

    chapter 6 - initialized Flutter project _05_textfield and implemented a login screen with form validation using TextFormField and TextEditingController

---
+++ ch_06/_05_textfield/lib/login_screen.dart
@@ -0,0 +1,113 @@
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  bool loggedIn = false;
+  String name = '';
+  String email = '';
+  final _nameController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  void dispose() {
+    _nameController.dispose();
+    _emailController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Login')),
+      body: Center(
+        child: loggedIn ? _buildSuccessControl() : _buildLoginForm(),
+      ),
+    );
+  }
+
+  Widget _buildSuccessControl() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Icon(Icons.check, color: Colors.orangeAccent),
+        Text('Hi $name'),
+      ],
+    );
+  }
+
+  Widget _buildLoginForm() {
+    return Form(
+      key: _formKey,
+      child: Padding(
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            _enterNameControl(),
+            _enterEmailControl(),
+            const SizedBox(height: 20),
+            _enterContinueButton(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _enterNameControl() {
+    return TextFormField(
+      controller: _nameController,
+      decoration: const InputDecoration(
+        hintText: 'Enter the runner\'s name',
+        labelText: 'Runner',
+      ),
+      validator: (text) {
+        return text!.isEmpty ? 'Enter runner name' : null;
+      },
+    );
+  }
+
+  Widget _enterEmailControl() {
+    return TextFormField(
+      controller: _emailController,
+      decoration: const InputDecoration(
+        hintText: 'Enter the runner\'s email',
+        labelText: 'Email',
+      ),
+      validator: (text) {
+        if (text!.isEmpty) {
+          return 'Enter the runner\'s email';
+        }
+        final regex = RegExp('[^@]+@[^.]+..+');
+        if (!regex.hasMatch(text)) {
+          return 'Enter a valid email';
+        }
+        return null;
+      },
+    );
+  }
+
+  Widget _enterContinueButton() {
+    return ElevatedButton(onPressed: _validate, child: const Text('Continue'));
+  }
+
+  void _validate() {
+    final form = _formKey.currentState;
+    if (!(form?.validate() ?? false)) {
+      return;
+    }
+    setState(() {
+      loggedIn = true;
+      name = _nameController.text;
+      email = _emailController.text;
+    });
+  }
+}
