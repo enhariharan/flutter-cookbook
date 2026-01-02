f7c880c23d65fc40cc3b9169599c33e4fcad4008
Hariharan Narayanan <enhariharan@gmail.com>
2026-01-02 13:17:32 +0530

    chapter 8 - initialized Flutter project _04_future_group and implemented asynchronous operations using FutureGroup, Completer, and custom Future delayed methods

---
+++ ch_08/_04_future_group/android/app/src/main/kotlin/com/example/u_04_future_group/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.u_04_future_group
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ ch_08/_04_future_group/lib/main.dart
@@ -0,0 +1,129 @@
+import 'dart:async';
+
+import 'package:async/async.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Future Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: const FuturePage(),
+    );
+  }
+}
+
+class FuturePage extends StatefulWidget {
+  const FuturePage({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _FuturePageState();
+}
+
+class _FuturePageState extends State<FuturePage> {
+  String result = '';
+  late Completer completer;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Back from the future')),
+      body: Center(
+        child: Column(
+          children: [
+            const Spacer(),
+            ElevatedButton(
+                onPressed: () => returnFutureFutureGroup(), child: const Text('Go!')),
+            const Spacer(),
+            Text(result),
+            const Spacer(),
+            const CircularProgressIndicator(),
+            const Spacer(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _onGoButtonClicked() => getNumber()
+      .then((value) => setState(() => result = value.toString()))
+      .catchError((e) => setState(() => result = 'An error occurred'))
+  ;
+
+  Future<Response> getData() async {
+    const authority = 'www.googleapis.com';
+    const path = '/books/v1/volumes/junbDwAAQBAJ';
+    Uri url = Uri.https(authority, path);
+    return get(url);
+  }
+
+  Future<int> returnOneAsync() async {
+    await Future.delayed(const Duration(seconds: 3));
+    debugPrint('1');
+    return 1;
+  }
+
+  Future<int> returnTwoAsync() async {
+    await Future.delayed(const Duration(seconds: 3));
+    debugPrint('2');
+    return 2;
+  }
+
+  Future<int> returnThreeAsync() async {
+    await Future.delayed(const Duration(seconds: 3));
+    debugPrint('3');
+    return 3;
+  }
+
+  Future count() async {
+    int total = 0;
+    total = await returnOneAsync();
+    total += await returnTwoAsync();
+    total += await returnThreeAsync();
+    setState(() {
+      result = total.toString();
+      debugPrint('result: $result');
+    });
+  }
+
+  Future getNumber() {
+    completer = Completer<int>();
+    calculate();
+    return completer.future;
+  }
+
+  Future calculate() async {
+    try {
+      // throw Exception();
+      await Future.delayed(const Duration(seconds: 5));
+      completer.complete(42);
+    } catch (_) {
+      completer.completeError({});
+    }
+  }
+
+  void returnFutureFutureGroup() {
+    FutureGroup<int> futureGroup = FutureGroup<int>();
+    futureGroup.add(returnOneAsync());
+    futureGroup.add(returnTwoAsync());
+    futureGroup.add(returnThreeAsync());
+    futureGroup.close();
+    futureGroup.future.then(
+            (List<int> value) {
+          int total = 0;
+          for (var e in value) {
+            total += e;
+          }
+          setState(() => result = total.toString());
+        });
+  }
+}
