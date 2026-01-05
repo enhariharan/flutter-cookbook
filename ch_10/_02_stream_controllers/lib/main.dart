63bbc78b0f2bb0a5d4c61e687ec29a8d09271828
Hariharan Narayanan <enhariharan@gmail.com>
2026-01-05 08:17:53 +0530

    chapter 10 - initialized Flutter project `_02_stream_controllers` and implemented a random number generator demo utilizing `StreamController` and `sink` to update UI state; also included `ColorStream` and `NumberStream` classes in a dedicated persistence/logic layer.

---
+++ ch_10/_02_stream_controllers/lib/main.dart
@@ -0,0 +1,82 @@
+import 'dart:async';
+import 'dart:math';
+
+import 'stream.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(const StreamDemoApp());
+
+class StreamDemoApp extends StatelessWidget {
+  const StreamDemoApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Stream Demo',
+      theme: ThemeData(
+        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const StreamDemoHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class StreamDemoHomePage extends StatefulWidget {
+  const StreamDemoHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<StreamDemoHomePage> createState() => _StreamDemoHomePageState();
+}
+
+class _StreamDemoHomePageState extends State<StreamDemoHomePage> {
+  int _lastNumber = 0;
+  late StreamController<int> _numberStreamController;
+  late NumberStream _numberStream;
+
+  @override
+  void initState() {
+    _numberStream = NumberStream();
+    _numberStreamController = _numberStream.controller;
+    Stream stream = _numberStreamController.stream;
+    stream.listen((event) => setState(() => _lastNumber = event));
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _numberStream.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(_lastNumber.toString()),
+            ElevatedButton(
+                onPressed: ()  => addRandomNumber(),
+                child: Text('New Random Number')
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void addRandomNumber() {
+    Random random = Random();
+    int num = random.nextInt(100);
+    _numberStream.addNumberToSink(num);
+  }
+}
