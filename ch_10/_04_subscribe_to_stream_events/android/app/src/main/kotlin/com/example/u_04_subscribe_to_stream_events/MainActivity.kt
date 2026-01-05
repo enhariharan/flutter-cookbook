f45bf3593d8bb5b59c7de2f1896ebb052034ff35
Hariharan Narayanan <enhariharan@gmail.com>
2026-01-05 10:25:25 +0530

    chapter 10 - initialized Flutter project `_03_transform_data_in_stream` and implemented a stream data transformation example using `StreamTransformer` to multiply incoming integers by 10 before displaying them in the UI.

---
+++ ch_10/_04_subscribe_to_stream_events/android/app/src/main/kotlin/com/example/u_04_subscribe_to_stream_events/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.u_04_subscribe_to_stream_events
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ ch_10/_04_subscribe_to_stream_events/lib/main.dart
@@ -0,0 +1,99 @@
+import 'dart:async';
+import 'dart:math';
+
+import 'stream.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(const StreamDemoApp());
+
+class StreamDemoApp extends StatelessWidget {
+  const StreamDemoApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Stream Demo',
+      theme: ThemeData(
+        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const StreamDemoHomePage(title: 'Flutter Stream Demo Home Page'),
+    );
+  }
+}
+
+class StreamDemoHomePage extends StatefulWidget {
+  const StreamDemoHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<StreamDemoHomePage> createState() => _StreamDemoHomePageState();
+}
+
+class _StreamDemoHomePageState extends State<StreamDemoHomePage> {
+  late StreamSubscription _subscription;
+  int _lastNumber = 0;
+  late StreamController<int> _numberStreamController;
+  late NumberStream _numberStream;
+
+  late StreamTransformer _transformer;
+
+  @override
+  void initState() {
+    _numberStream = NumberStream();
+    _numberStreamController = _numberStream.controller;
+    Stream stream = _numberStreamController.stream;
+    _subscription = stream
+        .listen((event) => setState(() => _lastNumber = event));
+    _subscription.onError((error) => setState(() => _lastNumber = -1));
+    _subscription.onDone(() => print('OnDone() was called'));
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _numberStream.dispose();
+    _subscription.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(_lastNumber.toString()),
+            ElevatedButton(
+                onPressed: ()  => addRandomNumber(),
+                child: Text('New Random Number')
+            ),
+            ElevatedButton(
+                onPressed: ()  => stopStream(),
+                child: Text('Stop subscription')
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void addRandomNumber() {
+    Random random = Random();
+    int num = random.nextInt(100);
+    if (!_numberStreamController.isClosed) {
+      _numberStream.addNumberToSink(num);
+    } else {
+      setState(() => _lastNumber = -1);
+    }
+  }
+
+  void stopStream() => _numberStreamController.close();
+}
