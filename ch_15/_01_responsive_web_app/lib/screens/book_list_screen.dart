ee3e7e011e99b884105ede4c0905d395ec48ac37
Hariharan Narayanan <enhariharan@gmail.com>
2026-01-08 07:40:04 +0530

    Update `responsive_web_app` example to support item highlighting (#12345)
    - Update `MaterialApp` title to 'Flutter Mobile App Demo'.
    - Add background color state tracking for book list items.
    - Wrap `ListTile` items in a `GestureDetector` and `ColoredBox` to handle tap interactions.
    - Implement `setColor` to toggle item background colors on tap, secondary tap, and long press.

--- ch_15/_01_responsive_web_app/lib/screens/book_list_screen.dart
+++ ch_15/_01_responsive_web_app/lib/screens/book_list_screen.dart
@@ -11,15 +11,22 @@ class BookListScreen extends StatefulWidget {
 }
 
 class _BookListScreenState extends State<BookListScreen> {
+  List<Color> _bgColors = [];
   List<Book> _books = [];
 
   @override
   void initState() {
     super.initState();
     final HttpHelper httpHelper = HttpHelper();
-    httpHelper.getBooks('flutter').then((List<Book> books) {
-      setState(() => _books = books);
-    });
+    httpHelper
+        .getBooks('flutter')
+        .then((List<Book> books) {
+          setState(() => _books = books);
+          int i;
+          for (i=0; i<books.length; i++) {
+            _bgColors.add(Colors.white);
+          }
+        });
   }
 
   @override
@@ -31,14 +38,27 @@ class _BookListScreenState extends State<BookListScreen> {
       body: GridView.count(
         childAspectRatio: isLargeScreen ? 8 : 5,
         crossAxisCount: isLargeScreen ? 2 : 1,
-        children: List.generate(_books.length, (index) => ListTile(
-        title: Text(_books[index].title),
-        subtitle: Text(_books[index].authors),
-        leading: CircleAvatar(
-          backgroundImage: (_books[index].thumbnail.isEmpty) ? null : NetworkImage(_books[index].thumbnail),
-        ),
+        children: List.generate(_books.length, (index) => GestureDetector(
+          onTap: () => setColor(Colors.blue, index),
+          onSecondaryTap: () => setColor(Colors.white, index),
+          onLongPress: () => setColor(Colors.white, index),
+          child: ColoredBox(
+            color: _bgColors.isNotEmpty ? _bgColors[index] : Colors.white,
+            child: ListTile(
+            title: Text(_books[index].title),
+            subtitle: Text(_books[index].authors),
+            leading: CircleAvatar(
+              backgroundImage: (_books[index].thumbnail.isEmpty) ? null : NetworkImage(_books[index].thumbnail),
+            ),
+            ),
+          ),
         )),
       ),
     );
   }
+
+  void setColor(Color color, int index) {
+    setState(() => _bgColors[index] = color);
+  }
+
 }
